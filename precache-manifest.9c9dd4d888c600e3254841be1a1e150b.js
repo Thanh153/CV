5e1a22ab3b00a10e20932904e6f8fc3d60770cdd
Thanh153 <kimthanh99315@gmail.com>
2020-10-22 21:56:36 +0700

    Updates

---
+++ precache-manifest.9c9dd4d888c600e3254841be1a1e150b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ba6167133ca281fddff42ee489f395",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "b01d39655fc06ccff099",
+    "url": "/CV/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "81314442eff1d58628ef",
+    "url": "/CV/static/css/main.60211a9b.chunk.css"
+  },
+  {
+    "revision": "b01d39655fc06ccff099",
+    "url": "/CV/static/js/2.f0a94e04.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/CV/static/js/2.f0a94e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81314442eff1d58628ef",
+    "url": "/CV/static/js/main.0904cc4c.chunk.js"
+  },
+  {
+    "revision": "e4d273214469930d29bd",
+    "url": "/CV/static/js/runtime-main.4d0d12ac.js"
+  },
+  {
+    "revision": "3f529d0c7a60251f0331223e75fb8035",
+    "url": "/CV/static/media/profile.3f529d0c.png"
+  }
+]);
